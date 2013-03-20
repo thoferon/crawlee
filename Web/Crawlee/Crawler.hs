42dc10722261f1c728a5bd9db4dbac458be1d19b
Thomas Feron <t.feron@nature.com>
2013-03-20 14:48:15 +0000

    It compiles

--- Web/Crawlee/Crawler.hs
+++ Web/Crawlee/Crawler.hs
@@ -1,33 +1,42 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification, MultiParamTypeClasses, FlexibleInstances #-}
 
 module Web.Crawlee.Crawler (
-  Crawler(..)
-  , Queue
-  , Url
+  Url
+  , UrlQueue
   , Page
-  , f
+  , UniqQueue(..)
+  , Crawler(..)
+  , Queue
+  , getResults
   ) where
 
 import Data.Monoid
 import Control.Monad
 
-data UniqQueue a = UniqQueue a
-data Crawler q i r = (Queue (q i), Monoid r) => Crawler { scrap :: (i -> (q i, r)) }
+type Url = String
+type UrlQueue = UniqQueue Url
+data Page = Page { url :: Url, content :: String }
 
-class Monoid q => Queue q where
-  first :: q -> i
-  rest  :: q -> q
+data UniqQueue a = UniqQueue [a]
+data Crawler q i r = (Monoid (q i), Monoid r) => Crawler { scrap :: (i -> (q i, r)) }
+
+class Queue q i where
+  first :: q i -> Maybe i
+  rest  :: q i -> q i
 
 instance Monoid a => Monoid (UniqQueue a) where
   mempty = UniqQueue mempty
   mconcat _ = undefined
   UniqQueue a `mappend` UniqQueue b = UniqQueue $ a `mappend` b
 
-instance Queue (UniqQueue a) where
-  first (UniqQueue (i:_)) = i
+instance Queue UniqQueue a where
+  first (UniqQueue []) = Nothing
+  first (UniqQueue (i:_)) = Just i
   rest (UniqQueue is) = UniqQueue $ tail is
 
-getResults :: (Queue (q i), Monoid r) => Crawler q i r -> q i -> r
-getResults (Crawler scrap) queue
-  | queue == mempty = mempty
-  | otherwise = mempty
+getResults :: (Queue q i, Monoid (q i), Monoid r) => Crawler q i r -> q i -> r
+getResults c q =
+  case first q of
+    Nothing -> mempty
+    Just i -> let (q', r) = scrap c i
+              in r `mappend` getResults c (rest q `mappend` q')
